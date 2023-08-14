ce35b79e09d60fb86f68d3816e3b013554966ca1
Tarun Mittal <tm6622@g.rit.edu>
2023-08-13 21:34:53 -0400

    modified codee

--- Backend/app.py
+++ Backend/app.py
@@ -1,31 +1,38 @@
 from flask import Flask, request, jsonify
-from google.cloud import speech
-from google.cloud.speech import enums, types
+from flask_cors import CORS
+from google.cloud import speech_v1 as speech
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/voice-to-text", methods=["POST"])
 def voice_to_text():
-    audio_file = request.files['file']
-    
-    client = speech.SpeechClient()
+    try:
+        audio_file = request.files["file"]
 
-    content = audio_file.read()
-    audio = types.RecognitionAudio(content=content)
-    config = types.RecognitionConfig(
-        encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
-        sample_rate_hertz=16000,
-        language_code='en-US',
-    )
+        client = speech.SpeechClient()
 
-    response = client.recognize(config=config, audio=audio)
+        content = audio_file.read()
+        audio = speech.RecognitionAudio(content=content)
+        config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+            sample_rate_hertz=16000,
+            language_code="en-US",
+        )
 
-    if not response.results:
-        return jsonify({"error": "Could not understand audio"}), 400
+        response = client.recognize(config=config, audio=audio)
 
-    transcript = response.results[0].alternatives[0].transcript
+        if not response.results:
+            return jsonify({"error": "Could not understand audio"}), 400
 
-    return jsonify({"transcript": transcript})
+        transcript = response.results[0].alternatives[0].transcript
+
+        return jsonify({"transcript": transcript})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
